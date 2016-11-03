6daaf91e0fe26bb662a5e3de42b303ecbf16d964
jacob <jacob.miller@us.qbe.com>
2016-11-03 12:31:26 -0500

    got it to compile

--- chaincode_start.go
+++ chaincode_start.go
@@ -5,15 +5,22 @@ import(
 	"errors"
 	"fmt"
 	"strconv"
-//	"time"
-//	"string"
+	"time"
+	
+	"github.com/satori/go.uuid"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 type SimpleChaincode struct {}
 
+type PolicyHolder struct {
+	ID string
+	Policies []Policy
+}
+
 type CarrierTerms struct {
 	CarrierID string
+	Timestamp int64
 	Country string
 	Premium int32
 	Value int32
@@ -21,12 +28,26 @@ type CarrierTerms struct {
 
 type Policy struct {
 	ID string
+	Timestamp int64
+	HolderID string
+	Countries []string
 	Terms []CarrierTerms
+}
+
+type AllPolicies struct {
+	Catalog []Policy
+}
+
+var pendingPoliciesString = "_pendingPolicies"
+var activePoliciesString = "_activePolicies"
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond))
 }
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error starting simple chaincode: %s", err)
 	}
 }
@@ -36,13 +57,19 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	fmt.Println("Initializing Policies")
 
 	// Encode empty array of strings into json
-	//var blank []string
-	blankBytes, _ := json.Marshal(&args[0])//json.Marshal(&blank)
+	var blank []string
+	blankBytes, _ := json.Marshal(&blank)
 
 	// Set the state of the 'Cows' variable to blank
-	err := stub.PutState("Policies", blankBytes)
+	err := stub.PutState(activePoliciesString, blankBytes)
 	if err != nil {
 		fmt.Println("Failed to initialize policies")
+		return nil, err
+	}
+
+	err = stub.PutState(pendingPoliciesString, blankBytes)
+	if err != nil {
+		fmt.Println("Failed to initialize pending policies")
 		return nil, err
 	}
 
@@ -55,12 +82,72 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 
 	if function == "init" {
 		return t.Init(stub, "init", args)
-	} else if function == "write" {
-		return t.Write(stub, args)
 	}
 	
 	fmt.Println("Invoke did not find a function: " + function)
 	return nil, errors.New("Received unknown function invocation")
+}
+
+func (t *SimpleChaincode) createTerms(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	/*
+json 
+{
+    "carrier_id" : string,
+    "country": string,
+    "premium":0.00,
+    "value" : 0.00
+}
+
+*/
+	return nil, nil
+}
+
+func (t *SimpleChaincode) initializePolicy(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	if len(args) < 2 {
+		return nil, errors.New("Expected multiple arguments; arguments received: " +  strconv.Itoa(len(args)))
+	}
+
+	// Generate a new UUID for the new policy
+	policyID := uuid.NewV4().String()
+
+	holderID := args[0]
+	
+	// Compile the list of countries for which the policy needs to be covered
+	countries := make([]string, len(args) - 1)
+	i := 1
+	for i < len(args) {
+		countries[i] = args[i - 1]
+		i = i + 1
+	}
+
+	// Generate new policy object
+	var newPolicy Policy
+	newPolicy.ID = policyID
+	newPolicy.Timestamp = makeTimestamp()
+	newPolicy.HolderID = holderID
+	newPolicy.Countries = countries
+	newPolicy.Terms = nil
+
+	// Retrieve the current list of pending policies
+	pendingAsBytes, err := stub.GetState(pendingPoliciesString)
+	if err != nil {
+		return nil, errors.New("Failed to get pending policies")
+	}
+
+	var pendingPolicies AllPolicies
+	json.Unmarshal(pendingAsBytes, &pendingPolicies)
+
+	// Add the new policy to the list of pending policies
+	pendingPolicies.Catalog = append(pendingPolicies.Catalog, newPolicy)
+	fmt.Println("New policy appended to pending policies. Pending policy count: " + strconv.Itoa(len(pendingPolicies.Catalog)))
+
+	pendingAsBytes, _ = json.Marshal(pendingPolicies)
+	err = stub.PutState(pendingPoliciesString, pendingAsBytes)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Policy successfully added to pending policies")
+	return nil, nil
 }
 
 // Check the state of the chaincode
@@ -68,34 +155,14 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	fmt.Println("Querying: " + function)
 
 	if function == "read" {
-		return t.Read(stub, args)
+		return t.read(stub, args)
 	}
 	fmt.Println("Query did not find a function: " + function)
 	return nil, errors.New("Received unknown function query")
 }
 
-// Write a value to a variable
-func (t *SimpleChaincode) Write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	fmt.Println("Writing")
-
-	var name, value string
-	var err error
-
-	if len(args) != 2 {
-		return nil, errors.New("Expecting two arguments; arguments received: " + strconv.Itoa(len(args)))
-	}
-
-	name = args[0]
-	value = args[1]
-	err = stub.PutState(name, []byte(value))
-	if err != nil {
-		return nil, err
-	}
-	return nil, nil
-}
-
 // Read the state of a variable
-func (t *SimpleChaincode) Read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var name, jsonResp string
 	var err error
 
