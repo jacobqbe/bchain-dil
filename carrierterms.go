8084b1fa4cfaf992496b5e3b7c5a69690c57960c
jacob <jacob.miller@us.qbe.com>
2016-11-07 14:25:52 -0600

    refactor and policies should now move from incomplete to pending once they have all necessary carriers

---
+++ carrierterms.go
@@ -0,0 +1,104 @@
+package main
+
+import(
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+type CarrierTerms struct {
+	CarrierID string `json:"carrier"`
+	ID string `json:"id"`
+	Country string `json:"country"`
+	Premium int64 `json:"premium"`
+	Value int64 `json:"value"`
+}
+
+func createTerms(args []string) (CarrierTerms, error) {
+	fmt.Println("Function: createTerms")
+	
+	var terms CarrierTerms
+	if len(args) != 4 {
+		return terms, errors.New("Expected 4 arguments; arguments received: " + strconv.Itoa(len(args)))
+	}
+
+	var err error
+	terms.CarrierID = args[0]
+	terms.ID = makeHash(args)
+	terms.Country = args[1] 
+	terms.Premium, err = strconv.ParseInt(args[2], 10, 64)
+	terms.Value, err = strconv.ParseInt(args[3], 10, 64)
+	
+	return terms, err
+}
+
+func assignTerms(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	fmt.Println("Function: assignTerms")
+	
+	// args should include the terms of the policy and the timestamp for the policy that it will be assigned to
+	if len(args) != 5 {
+		return nil, errors.New("Expecting 5 arguments; arguments received: " + strconv.Itoa(len(args)))
+	}
+
+	incompleteAsBytes, err := getPolicies(stub, incompletePoliciesString)
+	if err != nil {
+		return nil, err
+	}
+
+	var incompletePolicies AllPolicies
+	incompletePolicies, err = bytesToAllPolicies(incompleteAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(incompletePolicies.Catalog) == 0 {
+		return nil, errors.New("No incomplete policies were found")
+	}
+	
+	policyHash := args[0]
+	//var targetPolicy Policy
+	var index int
+	index, err = getPolicyByHash(incompletePolicies.Catalog, policyHash)
+	if err != nil {
+		return nil, err
+	}
+
+	carrierArgs := args[1:]
+	var carrierTerms CarrierTerms
+	carrierTerms, err = createTerms(carrierArgs)
+	if err != nil {
+		return nil, err
+	}
+
+	err = insertTermsIntoPolicy(&incompletePolicies.Catalog[index], carrierTerms)
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkComplete(incompletePolicies.Catalog[index])
+	if err != nil {
+		incompleteAsBytes, err = json.Marshal(incompletePolicies)
+		if err != nil {
+			return nil, err
+		}
+		err = write(stub, incompletePoliciesString, incompleteAsBytes)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("incomplete policies successfully written with new terms")
+		return nil, nil
+	}
+
+	// TODO(jacob): remove completed policy from incompletePolicies and insert into pendingPolicies
+	pendingPolicy := removePolicy(&incompletePolicies, index)
+	fmt.Println("pendingPolicy removed from incomplete policies")
+	err = addPendingPolicy(stub, pendingPolicy)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("policy successfully added to pending policies")
+	return nil, nil
+}
--- chaincode_start.go
+++ chaincode_start.go
@@ -1,41 +1,14 @@
 package main
 
 import(
-	//"bytes"
 	"encoding/json"
-	//"encoding/binary"
 	"errors"
 	"fmt"
-	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 type SimpleChaincode struct {}
-
-type PolicyHolder struct {
-	ID string `json:"id"`
-	Policies []Policy `json:"policies"`
-}
-
-type CarrierTerms struct {
-	CarrierID string `json:"carrier"`
-	ID string `json:"id"`
-	Country string `json:"country"`
-	Premium int64 `json:"premium"`
-	Value int64 `json:"value"`
-}
-
-type Policy struct {
-	ID string `json:"id"`
-	HolderID string `json:"holderID"`
-	Countries []string `json:"countries"`
-	Terms []CarrierTerms `json:"terms"`
-}
-
-type AllPolicies struct {
-	Catalog []Policy `json:"policies"`
-}
 
 var incompletePoliciesString = "_incompletePolicies"
 var pendingPoliciesString = "_pendingPolicies"
@@ -55,23 +28,14 @@ func makeHash(args []string) string {
 	return s
 }
 
-var logger = shim.NewLogger("debug log")
-
 func main() {
 	fmt.Println("Function: main")
 	
-	err := shim.Start(new(SimpleChaincode))
-	logger.SetLevel(shim.LogDebug)
-	shim.SetLoggingLevel(shim.LogDebug)
-	
+	err := shim.Start(new(SimpleChaincode))	
 	if err != nil {
 		fmt.Printf("Error starting simple chaincode: %s", err)
 	}
 }
-
-/*
-Methods for SimpleChaincode 
-*/
 
 // Initialize the state of the 'Policies' variable
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
@@ -130,7 +94,6 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	return nil, nil
 }
 
-// Manipulate the blockchain
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("Method: SimpleChaincode.Invoke; received: " + function)
 
@@ -146,7 +109,6 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	return nil, errors.New("Received unknown function invocation")
 }
 
-// Check the state of the blockchain
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("Method: SimpleChaincode.Query; received: " + function)
 
@@ -162,181 +124,6 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	return nil, errors.New("Received unknown function query")
 }
 
-/*
-Functions for carrying out complex actions on the blockchain.
-These include adding values to arrays on the blockchain and verification of permissions
-*/
-
-func generatePolicy(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	fmt.Println("Function: generatePolicy")
-
-	if len(args) < 2 {
-		return nil, errors.New("Expected multiple arguments; arguments received: " +  strconv.Itoa(len(args)))
-	}
-	newPolicy := createPolicyObject(args)
-	
-	// Retrieve the current list of pending policies
-	incompleteAsBytes, err := getPolicies(stub, incompletePoliciesString)
-	if err != nil {
-		return nil, err
-	}
-
-	var incompletePolicies AllPolicies
-	incompletePolicies, err = bytesToAllPolicies(incompleteAsBytes)
-	if err != nil {
-		return nil, err
-	}
-	
-	// Add the new policy to the list of pending policies
-	incompletePolicies.Catalog = append(incompletePolicies.Catalog, newPolicy)
-	fmt.Println("New policy appended to incomplete policies. Incomplete policy count: " + strconv.Itoa(len(incompletePolicies.Catalog)))
-
-	incompleteAsBytes, err = json.Marshal(incompletePolicies)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("incomplete policies successfully converted to bytes")
-	
-	err = write(stub, incompletePoliciesString, incompleteAsBytes)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("incomplete policies successfully rewritten with new policy")
-	return nil, nil
-}
-
-func assignTerms(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	fmt.Println("Function: assignTerms")
-	
-	// args should include the terms of the policy and the timestamp for the policy that it will be assigned to
-	if len(args) != 5 {
-		return nil, errors.New("Expecting 5 arguments; arguments received: " + strconv.Itoa(len(args)))
-	}
-
-	incompleteAsBytes, err := getPolicies(stub, incompletePoliciesString)
-	if err != nil {
-		return nil, err
-	}
-
-	var incompletePolicies AllPolicies
-	incompletePolicies, err = bytesToAllPolicies(incompleteAsBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(incompletePolicies.Catalog) == 0 {
-		return nil, errors.New("No incomplete policies were found")
-	}
-	
-	policyHash := args[0]
-	//var targetPolicy Policy
-	var index int
-	index, err = getPolicyByHash(incompletePolicies.Catalog, policyHash)
-	if err != nil {
-		return nil, err
-	}
-
-	carrierArgs := args[1:]
-	var carrierTerms CarrierTerms
-	carrierTerms, err = createTerms(carrierArgs)
-	if err != nil {
-		return nil, err
-	}
-
-	err = insertTermsIntoPolicy(&incompletePolicies.Catalog[index], carrierTerms)
-	if err != nil {
-		return nil, err
-	}
-
-	err = checkComplete(incompletePolicies.Catalog[index])
-	if err != nil {
-		incompleteAsBytes, err = json.Marshal(incompletePolicies)
-		if err != nil {
-			return nil, err
-		}
-		err = write(stub, incompletePoliciesString, incompleteAsBytes)
-		if err != nil {
-			return nil, err
-		}
-		fmt.Println("incomplete policies successfully written with new terms")
-		return nil, nil
-	}
-
-	// TODO(jacob): remove completed policy from incompletePolicies and insert into pendingPolicies
-	err = removePolicy(&incompletePolicies, index)
-	return nil, nil
-}
-
-func removePolicy(policies *AllPolicies, index int) error {
-	fmt.Println("Function: removePolicy")
-	return nil
-}
-
-func bytesToAllPolicies(policiesAsBytes []byte) (AllPolicies, error) {
-	fmt.Println("Function: bytesToAllPolicies")
-	
-	var policies AllPolicies
-
-	err := json.Unmarshal(policiesAsBytes, &policies)
-	fmt.Println("json.Unmarshal error:")
-	fmt.Println(err)
-	
-	return policies, err
-}
-
-func checkComplete(policy Policy) error {
-	fmt.Println("Function: checkComplete")
-
-	i := 0
-	for i < len(policy.Terms) {
-		if policy.Terms[i].ID == "" {
-			return errors.New("Policy incomplete")
-		}
-		i = i + 1
-	}
-	fmt.Println("Policy complete")
-	return nil
-}
-
-func getPolicyByHash(policies []Policy, hash string) (int, error) {
-	fmt.Println("Function: getPolicyByHash")
-	
-	var i int
-	i = 0
-	for i < len(policies) {
-		if policies[i].ID == hash {
-			return i, nil
-		}
-		i = i + 1
-	}
-	
-	return 0, errors.New("No policy found with hash: " + hash)
-}
-
-func insertTermsIntoPolicy(policy *Policy, terms CarrierTerms) error {
-	fmt.Println("Function: insertTermsIntoPolicy")
-	fmt.Println("country: " + terms.Country)
-	i := 0
-	for i < len(policy.Terms) {
-		fmt.Println("policy.Terms.Country: " + policy.Terms[i].Country)
-		if policy.Terms[i].Country == terms.Country {
-			fmt.Println(policy.Terms[i].ID) //
-			if policy.Terms[i].ID == "" {
-				policy.Terms[i] = terms
-				fmt.Println("Policy found; terms inserted")
-				fmt.Println("Carrier: " + policy.Terms[i].CarrierID);
-				return nil
-			}
-		}
-		i = i + 1
-	}
-	return errors.New("Policy does not require country: " + terms.Country)
-}
-
-/*
-Functions for directly reading & writing to the blockchain
-*/
-
 func write(stub *shim.ChaincodeStub, name string, value []byte) error {
 	fmt.Println("Function: write")
 	
@@ -345,58 +132,4 @@ func write(stub *shim.ChaincodeStub, name string, value []byte) error {
 		return err
 	}
 	return nil
-}
-
-func getPolicies(stub *shim.ChaincodeStub, policiesString string) ([]byte, error) {
-	fmt.Println("Function: getPolicies (" + policiesString + ")")
-	
-	policiesAsBytes, err := stub.GetState(policiesString)
-	if err != nil {
-		jsonResp := "{\"Error\": \"Failed to get policies.\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return policiesAsBytes, nil
-}
-
-/*
-Functions for creating objects 
-*/
-
-func createTerms(args []string) (CarrierTerms, error) {
-	fmt.Println("Function: createTerms")
-	
-	var terms CarrierTerms
-	if len(args) != 4 {
-		return terms, errors.New("Expected 4 arguments; arguments received: " + strconv.Itoa(len(args)))
-	}
-
-	var err error
-	terms.CarrierID = args[0]
-	terms.ID = makeHash(args)
-	terms.Country = args[1] 
-	terms.Premium, err = strconv.ParseInt(args[2], 10, 64)
-	terms.Value, err = strconv.ParseInt(args[3], 10, 64)
-	
-	return terms, err
-}
-
-func createPolicyObject(args []string) Policy {
-	fmt.Println("Function: createPolicyObject")
-	
-	var policy Policy
-	policy.ID = makeHash(args)
-	policy.HolderID = args[0]
-
-	countries := args[1:]
-	policy.Countries = countries
-	policy.Terms = make([]CarrierTerms, len(countries))
-
-	var i = 0
-	for i < len(countries){
-		policy.Terms[i].Country = countries[i]
-		i = i + 1
-	}
-
-	return policy
 }
